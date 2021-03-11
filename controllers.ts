fadbf5fc92e5089dccd040123d3ecff33407b0da
kirinokirino <kirinokirino2501@gmail.com>
2021-03-11 18:06:05 +0300

    Adds controllers interfaces, Adds about page controller.

--- controllers.ts
+++ controllers.ts
@@ -9,9 +9,30 @@ export const allowVerbs = function (
   };
 };
 
-export const helloWorld = function (
+export const aboutPage = function (
   { response }: { response: Response },
 ) {
   response.status = 200;
-  response.body = "<html><body>Hello world</body></html>";
+  response.body =
+    `<html><Head><Title>About Notes api</Title></Head><body><h2>Notes api routes:</h2>
+  <ul><li>/about - GET (this page)</li><li>/notes - GET, POST</li><li>/notes/:id - GET, PUT, DELETE</li></ul>
+  </body></html>`;
 };
+
+export const getAllNotes = function ({ response }: { response: Response }) {};
+export const postNote = function (
+  { request, response }: { request: Request; response: Response },
+) {};
+export const getNote = function (
+  { response, params }: { response: Response; params: { id: string } },
+) {};
+export const updateNote = function (
+  { request, response, params }: {
+    request: Request;
+    response: Response;
+    params: { id: string };
+  },
+) {};
+export const deleteNote = function (
+  { response, params }: { response: Response; params: { id: string } },
+) {};
