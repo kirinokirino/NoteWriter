602a470f6c1202cdfc36b8301eb743cb2aace0dd
kirinokirino <kirinokirino2501@gmail.com>
2021-03-11 14:24:35 +0300

    Adds notification when server starts listening.

--- deps.ts
+++ deps.ts
@@ -4,3 +4,4 @@ export {
   Response,
   Router,
 } from "https://deno.land/x/oak@v6.5.0/mod.ts";
+export * as log from "https://deno.land/std@0.90.0/log/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from "./deps.ts";
+import { log } from "./deps.ts";
 
 const router = new Router();
 
@@ -6,5 +7,11 @@ const app = new Application();
 const port = 8080;
 
 app.use(router.routes());
+
+app.addEventListener("listen", ({ secure, hostname, port }) => {
+  const protocol = secure ? "https://" : "http://";
+  const url = `${protocol}${hostname ?? "localhost"}:${port}`;
+  log.info(`Listening on: ${url}`);
+});
 
 await app.listen({ port });
